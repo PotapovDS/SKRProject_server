3b40751a5632e5938757687b072addf011bfd6a5
unknown <potapovdenis09@gmail.com>
2024-01-09 21:15:00 +0300

    first commit maven init

---
+++ src/main/java/com/project/SKRProject_server/SkrProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.SKRProject_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkrProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkrProjectServerApplication.class, args);
+	}
+
+}
